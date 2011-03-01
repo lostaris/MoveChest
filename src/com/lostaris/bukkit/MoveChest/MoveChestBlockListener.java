dcf66c0196f4350b63b96aba1fffc24003c5997c
lostaris <raver.omel@gmail.com>
2011-03-02 03:49:21 +1300

    removed the try catch

--- src/com/lostaris/bukkit/MoveChest/MoveChestBlockListener.java
+++ src/com/lostaris/bukkit/MoveChest/MoveChestBlockListener.java
@@ -41,10 +41,10 @@ public class MoveChestBlockListener extends BlockListener {
 				if (plugin.isFromSet()) {
 					Block block = event.getBlock();
 					source = (Chest) block.getState();
-					
+
 					// copy the contents of the source chest
 					items = source.getInventory().getContents();
-					
+
 					player.sendMessage("copied chest");
 					// flags to show we have copied
 					plugin.setFromSet(false);
@@ -55,32 +55,37 @@ public class MoveChestBlockListener extends BlockListener {
 					destination = (Chest) block.getState();
 					Inventory destInven = destination.getInventory();
 					Inventory sourceInven = source.getInventory();
-					
+
 					/* This gives a null pointer exception if there wouldn't be any left over
 					 * Works fine if there would be any left over, but not if there isn't any left over
 					 * as the amounts moved double
 					 * eg pasting one sand would result in there being two sand
 					 */
-					try {
-						// for when we can't fit everything in the destination chest
-						// add to the destination chest and record what can't fit
-						HashMap<Integer, ItemStack>  leftOver = destInven.addItem(sourceInven.getContents());
+
+					// for when we can't fit everything in the destination chest
+					// add to the destination chest and record what can't fit
+					HashMap<Integer, ItemStack>  leftOver; // = destInven.addItem(sourceInven.getContents());
+					ItemStack[] items = sourceInven.getContents();
+					if (destInven.addItem(items).size() != 0) {
+						leftOver = destInven.addItem(sourceInven.getContents());
 						sourceInven.clear();
 						// add the left over back to the source chest
+						log.info("leftover amount= " +leftOver.size());
 						for (Entry<Integer, ItemStack> value: leftOver.entrySet()) {
 							sourceInven.addItem(value.getValue());
+							log.info(value.getValue().toString());
 						}
 						player.sendMessage("destination chest full, leaving some behind");
-					} catch (NullPointerException e){
+					} else {
 						// for when we can fit everything in the destination chest
-						
+
 						/* if a new itemstack is made here and added the correct amount is added
 						 * but if a itemstack from the array made from above is used it adds 2x as much
 						 * 
 						 * The below would work here inserting the correct amount.
 						 */
 						//destInven.addItem(new ItemStack(1, 1));
-						
+
 						// but this doubles the amount inserted
 						for (int i =0; i<items.length; i++) {
 							if (items[i].getType() != Material.AIR) {
@@ -90,7 +95,7 @@ public class MoveChestBlockListener extends BlockListener {
 								log.warning("pasted " + items[i].toString());
 							}
 						}
-						
+
 						//clear the source chest as we have copied from it
 						sourceInven.clear();
 					}
@@ -98,7 +103,7 @@ public class MoveChestBlockListener extends BlockListener {
 					//update the chests with the new values
 					source.update();
 					destination.update();
-					
+
 					//change the copy flags to show we are done
 					plugin.setCopy(false);
 					plugin.setToSet(false);
