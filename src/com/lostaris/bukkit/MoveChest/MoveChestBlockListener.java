5d92f6f3affb6b5d8127a9e56a8453ad8eb4d9a5
lostaris <raver.omel@gmail.com>
2011-03-02 01:55:56 +1300

    cleaned code

--- src/com/lostaris/bukkit/MoveChest/MoveChestBlockListener.java
+++ src/com/lostaris/bukkit/MoveChest/MoveChestBlockListener.java
@@ -4,17 +4,12 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map.Entry;
 import java.util.logging.Logger;
-
 import org.bukkit.block.Block;
-import org.bukkit.block.BlockFace;
 import org.bukkit.block.Chest;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
-import org.bukkit.event.block.BlockCanBuildEvent;
-import org.bukkit.event.block.BlockDamageEvent;
 import org.bukkit.event.block.BlockInteractEvent;
 import org.bukkit.event.block.BlockListener;
-import org.bukkit.event.block.BlockPhysicsEvent;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 
@@ -25,19 +20,12 @@ import org.bukkit.inventory.ItemStack;
 public class MoveChestBlockListener extends BlockListener {
 	private final MoveChest plugin;
 	public static final Logger log = Logger.getLogger("Minecraft");
-	Inventory source;
-	ArrayList<ItemStack> sourceItems = new ArrayList<ItemStack>();
-	Inventory destination;
+	Chest source;
+	Chest destination;
+	ItemStack items[];
 
 	public MoveChestBlockListener(final MoveChest plugin) {
 		this.plugin = plugin;
-	}
-
-	public void onBlockDamage(BlockDamageEvent event) {
-		Player player = event.getPlayer();
-		if (player.getItemInHand().getType() == Material.CHEST){
-
-		}
 	}
 
 	public void onBlockInteract(BlockInteractEvent event) {
@@ -49,56 +37,46 @@ public class MoveChestBlockListener extends BlockListener {
 				if (event.getEntity() instanceof Player) {
 					player = (Player) event.getEntity();
 				}
+
 				// copying
 				if (plugin.isFromSet()) {
 					Block block = event.getBlock();
-					Chest chest = (Chest) block.getState();
-					source = chest.getInventory();
-					for (ItemStack i : source.getContents()) {
-						if (i.getType() != Material.AIR) {
-							sourceItems.add(i);
-							//log.warning("copied " + i);
-						}
-					}
+					source = (Chest) block.getState();
+
+					items = source.getInventory().getContents();
 					plugin.setFromSet(false);
 					plugin.setCopy(true);
 					player.sendMessage("copied chest");
+					source.update();
 					// pasting
 				} else if(plugin.isToSet()) {
 					Block block = event.getBlock();
-					Chest chest = (Chest) block.getState();
-					destination = chest.getInventory();
+					destination = (Chest) block.getState();
+					Inventory destInven = destination.getInventory();
+					Inventory sourceInven = source.getInventory();
 
 					try {
-						HashMap<Integer, ItemStack>  leftOver = destination.addItem(source.getContents());
-						source.clear();
+						HashMap<Integer, ItemStack>  leftOver = destInven.addItem(sourceInven.getContents());
+						sourceInven.clear();
 						for (Entry<Integer, ItemStack> value: leftOver.entrySet()) {
-							source.addItem(value.getValue());
+							sourceInven.addItem(value.getValue());
 						}
 						player.sendMessage("destination chest full, leaving some behind");
-						sourceItems.clear();
 					} catch (NullPointerException e){
-//						ItemStack[] inven = source.getContents();
-//						log.warning("inven " + inven);
-//						for (ItemStack i : inven) {
-//							i.toString();
-//						}
-						
-						//destination.clear();
-						source.clear();
-						for ( ItemStack i : sourceItems) {
-							destination.addItem(i);
-							log.warning("pasted " + i);
+
+						for (int i =0; i<items.length; i++) {
+							if (items[i].getType() != Material.AIR) {
+								destInven.addItem(items[i]);
+								log.warning("pasted " + items[i].toString());
+							}
 						}
-						
-						//destination.addItem(inven);
-						//source.clear();
-						sourceItems.clear();						
 					}
+
+					source.update();
+					destination.update();
 					source = null;
 					destination = null;
-					
-					//chest.update();
+
 					plugin.setCopy(false);
 					plugin.setToSet(false);
 					player.sendMessage("moved contents of the chest.");
@@ -106,6 +84,4 @@ public class MoveChestBlockListener extends BlockListener {
 			}
 		}
 	}
-
-	//put all Block related code here
 }
